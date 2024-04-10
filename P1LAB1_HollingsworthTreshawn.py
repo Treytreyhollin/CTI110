002b8e0d2548a16755f238f213ff6d3c0f0bc449
Treytreyhollin <166655355+Treytreyhollin@users.noreply.github.com>
2024-04-10 16:24:58 -0400

    Add files via upload
    first submission

---
+++ P1LAB1_HollingsworthTreshawn.py
@@ -0,0 +1,9 @@
+
+#Treshawn Hollingsworth
+#April 1,2024
+#P1Lab1
+#This assignment is is a survey for the user that asks for first and last name
+
+firstname=input("Enter your first name:")
+lastname=input("Enter your last name:")
+print(f"Hello,{firstname} {lastname}! Welcome to CTI-110")
